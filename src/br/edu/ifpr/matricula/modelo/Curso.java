5e3801c4d54fcc83daf4df70f7af2ca6c3f9e6a8
Mauro <mauro.servise@gmail.com>
2024-08-01 20:22:14 -0300

    segundo commit

---
+++ src/br/edu/ifpr/matricula/modelo/Curso.java
@@ -0,0 +1,5 @@
+package br.edu.ifpr.matricula.modelo;
+
+public class Curso {
+
+}
---
+++ src/br/edu/ifpr/matricula/modelo/Turma.java
@@ -0,0 +1,8 @@
+package br.edu.ifpr.matricula.modelo;
+
+public class Turma {
+    String nome;
+    int numeroMinimo;
+    int anoIngresso;
+
+}
